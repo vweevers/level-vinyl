5f23ceca9f9e700d40915817715527b0f4c2bbe7
Vincent Weevers <mail@vincentweevers.nl>
2015-01-06 20:38:57 +0100

    create test dir

--- test/utils/create-db.js
+++ test/utils/create-db.js
@@ -3,10 +3,13 @@ var level = require('level-test')({ valueEncoding: 'json' })
   , sub = require('level-sublevel')
   , path = require('path')
   , tmpdir = require('osenv').tmpdir()
+  , mkdirp = require('mkdirp')
 
 module.exports = function() {
   var prefix = 'level-vinyl-tests/' + Date.now()
-  var blobs = path.join(tmpdir, prefix, 'blobs');
+  mkdirp.sync(path.join(tmpdir, prefix))
+
+  var blobs = path.join(tmpdir, prefix, 'vinyl-blobs');
   var sdb = sub(level(prefix+'/db'), { valueEncoding: 'json'});
   return levelVinyl(sdb, blobs)
 }
