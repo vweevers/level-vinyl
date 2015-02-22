f9df1f50c96356a2b1fed9584ce05cd774f661da
Vincent Weevers <mail@vincentweevers.nl>
2015-02-22 23:56:56 +0100

    use one db for tests

--- test/utils/create-db.js
+++ test/utils/create-db.js
@@ -4,12 +4,16 @@ var level = require('level-test')({ valueEncoding: 'json' })
   , path = require('path')
   , tmpdir = require('osenv').tmpdir()
   , mkdirp = require('mkdirp')
+  , prefix = 'level-vinyl-tests/' + Date.now()
+  , blobs = path.join(tmpdir, prefix, 'vinyl-blobs')
+
+mkdirp.sync(blobs)
+
+var ldb = level(prefix+'/db')
+  , sdb = sub(ldb, { valueEncoding: 'json' })
+  , vdb = levelVinyl(sdb, blobs)
+  , n   = 1
 
 module.exports = function() {
-  var prefix = 'level-vinyl-tests/' + Date.now()
-  mkdirp.sync(path.join(tmpdir, prefix))
-
-  var blobs = path.join(tmpdir, prefix, 'vinyl-blobs');
-  var sdb = sub(level(prefix+'/db'), { valueEncoding: 'json'});
-  return levelVinyl(sdb, blobs)
+  return vdb.subvinyl( '' + n++ )
 }
